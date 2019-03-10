d1684d3125a1ad5a722002c6d06dff717afbdae2
Nikolas Philips <nikolas.philips@gmail.com>
2019-03-10 01:17:20 +0100

    Add reload endpoint

--- src/main/java/sh/wheel/gitops/agent/api/GitHookRestController.java
+++ src/main/java/sh/wheel/gitops/agent/api/GitHookRestController.java
@@ -1,21 +1,31 @@
 package sh.wheel.gitops.agent.api;
 
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sh.wheel.gitops.agent.service.AgentService;
+import sh.wheel.gitops.agent.service.StateService;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 
 
 @RestController
 public class GitHookRestController {
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private AgentService agentService;
+    private StateService stateService;
 
     @Autowired
-    public GitHookRestController(AgentService agentService) {
+    public GitHookRestController(AgentService agentService, StateService stateService) {
         this.agentService = agentService;
+        this.stateService = stateService;
     }
 
     @RequestMapping("/hook")
@@ -29,8 +39,23 @@ public class GitHookRestController {
         return "Synchronization started...";
     }
 
+    @RequestMapping("/reload")
+    public String reload() {
+        this.callStateService();
+        return "Reload started...";
+    }
+
     @Async
     void callAgentService() {
         agentService.synchronize();
+    }
+
+    @Async
+    void callStateService() {
+        try {
+            stateService.init();
+        } catch (Exception e) {
+            LOG.warn("Error occured while calling StateService", e);
+        }
     }
 }
