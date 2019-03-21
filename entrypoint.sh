0dc91c5fcda39b3082fb80904f28137bef5006dc
Nikolas Philips <nikolas.philips@gmail.com>
2019-03-21 09:05:58 +0100

    Add memory settings to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 exec java ${JAVA_OPTS} \
+-XX:+UnlockExperimentalVMOptions \
+-XX:+UseCGroupMemoryLimitForHeap \
+-XX:MaxRAMFraction=1 \
 -Djava.security.egd=file:/dev/./urandom \
 -jar "/opt/app.jar" "$@"
